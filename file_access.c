d4d0c3e5a6474e5b68e00e9c4e1b7882fe114caf
Alexandra (Sasha)Fedorova <sasha@ece.ubc.ca>
2019-08-13 12:37:09 -0700

    Finished the mmap test

--- file_access.c
+++ file_access.c
@@ -1,4 +1,6 @@
+#include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -7,20 +9,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <unistd.h>
 
 #include "nano_time.h"
 
 #define DEFAULT_BLOCK_SIZE 4096
 
-char filename[] = "/mnt/pm/file";
+char default_fname[] = "/mnt/pm/file";
 
 uint64_t do_read_syscall_test(int fd, size_t block_size);
-
+uint64_t do_read_mmap_test(int fd, size_t block_size, const char *filename);
+size_t get_filesize(const char* filename);
 
 /**
- * For read tests, create a file prior to running tests using this command:
+ * For read tests, create a 4GB file prior to running tests using this command:
  *      $ dd < /dev/zero bs=1048576 count=4096 > testfile
  *
  * It will actually allocate space on disk.
@@ -40,7 +42,7 @@ int main(int argc, char **argv) {
 	if (argc > 1)
 		fname = argv[1];
 	else
-		fname = (char*) filename;
+		fname = (char*) default_fname;
 
 
 	fd = open((const char*)fname, O_RDWR | O_CREAT);
@@ -50,8 +52,10 @@ int main(int argc, char **argv) {
 	}
 
 	retval = do_read_syscall_test(fd, block_size);
-	printf("%" PRIu64 "\n",  retval);
-	//do_read_mmap_test(fd);
+	printf("\t Meaningless return token: %" PRIu64 "\n",  retval);
+
+	retval = do_read_mmap_test(fd, block_size, (const char*) fname);
+	printf("\t Meaningless return token: %" PRIu64 "\n",  retval);
 
 }
 
@@ -100,4 +104,69 @@ do_read_syscall_test(int fd, size_t block_size) {
 	       (double)total_bytes_read/(double)(end_time-begin_time));
 
 	return ret_token;
+}
+
+/**
+ *
+ *
+ */
+uint64_t
+do_read_mmap_test(int fd, size_t block_size, const char *filename) {
+
+	bool done = false;
+	char *mmapped_buffer = NULL;
+	int i, ret;
+	size_t filesize;
+	uint64_t begin_time, end_time, ret_token = 0;
+
+	if ((filesize = get_filesize(filename)) == -1)
+		return -1;
+
+#ifdef __MACH__
+	mmapped_buffer = (char *)mmap(NULL, filesize, PROT_READ,
+				      MAP_PRIVATE, fd, 0);
+#else /* Assumes Linux 2.6.23 or newer */
+	mmapped_buffer = (char *)mmap(NULL, filesize, PROT_READ,
+				      MAP_PRIVATE | MAP_POPULATE, fd, 0);
+#endif
+	if (mmapped_buffer == MAP_FAILED) {
+		printf("Failed to mmap file %s of size %" PRIu64 " : %s\n",
+		       filename, (uint_least64_t)filesize, strerror(errno));
+		return -1;
+	}
+
+	begin_time = nano_time();
+
+	for (i = 0; i < filesize; i += block_size)
+		ret_token += mmapped_buffer[i];
+
+	end_time = nano_time();
+
+	printf("read_mmap: %" PRIu64 " bytes read in %" PRIu64 " ns.\n",
+	       (uint_least64_t)filesize, (end_time-begin_time));
+	printf("\t %.2f bytes/second\n",
+	       (double)filesize/(double)(end_time-begin_time));
+
+	ret = munmap(mmapped_buffer, filesize);
+	if (ret)
+		printf("Error unmapping file %s: %s\n",
+		       filename, strerror(errno));
+
+	return ret_token;
+}
+
+size_t
+get_filesize(const char* filename) {
+
+	int retval;
+
+	struct stat st;
+	retval = stat(filename, &st);
+	if (retval) {
+		printf("Failed to find the size of file %s:"
+		       "%s\n", filename, strerror(errno));
+		return -1;
+	}
+	else
+		return st.st_size;
 }
