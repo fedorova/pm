12d8ba0225ba10b05393838d8147a44ee25a926a
Sasha Fedorova <sasha@mongodb.com>
2019-08-22 06:02:16 +1000

    Added options and help message.

--- file_access.c
+++ file_access.c
@@ -17,11 +17,12 @@
 
 #define DEFAULT_BLOCK_SIZE 4096
 
-char default_fname[] = "/mnt/pm/file";
+const char DEFAULT_FNAME[] = "/mnt/pmem/testfile";
 
 uint64_t do_read_syscall_test(int fd, size_t block_size);
-uint64_t do_read_mmap_test(int fd, size_t block_size, const char *filename);
+uint64_t do_read_mmap_test(int fd, size_t block_size, size_t filesize);
 size_t   get_filesize(const char* filename);
+void     print_help_message(const char* progname);
 
 /**
  * For read tests, create a 4GB file prior to running tests using this command:
@@ -36,25 +37,27 @@ size_t   get_filesize(const char* filename);
 
 int main(int argc, char **argv) {
 
-	char *fname = (char*) default_fname;
+	char *fname = (char*) DEFAULT_FNAME;
 	int c, fd, option_index;
 	static int read_mmap = 0, read_syscall = 0;
-	size_t block_size = DEFAULT_BLOCK_SIZE;
+	size_t block_size = DEFAULT_BLOCK_SIZE, filesize;
 	uint64_t retval;
 
 	static struct option long_options[] =
         {
 		/* These options set a flag. */
-		{"readmmap",   no_argument,   &read_mmap, 1},
+		{"readmmap", no_argument,   &read_mmap, 1},
 		{"readsyscall", no_argument,  &read_syscall, 1},
 		/* These options take an argument. */
-		{"file",    required_argument, 0, 'f'},
+		{"block", required_argument, 0, 'b'},
+		{"file", required_argument, 0, 'f'},
+		{"help", no_argument, 0, 'h'},
 		{0, 0, 0, 0}
 	};
 
 	/* Read long options */
 	while (1) {
-		c = getopt_long (argc, argv, "",
+		c = getopt_long (argc, argv, "b:f:h",
 				 long_options, &option_index);
 
 		/* Detect the end of the options. */
@@ -64,17 +67,17 @@ int main(int argc, char **argv) {
 		switch (c)
 		{
 		case 0:
-			/* If this option set a flag, do nothing else now. 
-			if (long_options[option_index].flag != 0)
-				break;
-			printf ("option %s", long_options[option_index].name);
-			if (optarg)
-				printf (" with arg %s", optarg);
-				printf ("\n"); */
+			/* If this option set a flag, do nothing else now. */
+			break;
+		case 'b':
+			block_size = atoi(optarg);
 			break;
 		case 'f':
 			fname = optarg;
 			break;
+		case 'h':
+			print_help_message(argv[0]);
+			_exit(0);
 		default:
 			break;
 		}
@@ -88,6 +91,16 @@ int main(int argc, char **argv) {
 	else
 		printf("Using file %s\n", fname);
 
+	if ((filesize = get_filesize(fname)) == -1)
+		_exit(-1);
+
+	if (block_size < 0 || block_size > filesize) {
+		printf("Invalid block size: %" PRIu64 " for file of size %" PRIu64 "."
+		       "Block size must be greater than zero and no greater than the "
+		       "file size.\n", block_size, filesize);
+		_exit(-1);
+	}
+	
 	if (read_syscall) {
 		printf("Running readsyscall test:\n");
 		retval = do_read_syscall_test(fd, block_size);
@@ -95,7 +108,7 @@ int main(int argc, char **argv) {
 	}
 	if (read_mmap) {
 		printf("Running readmmap test:\n");
-		retval = do_read_mmap_test(fd, block_size, (const char*) fname);
+		retval = do_read_mmap_test(fd, block_size, filesize);
 		printf("\t Meaningless return token: %" PRIu64 "\n",  retval);
 	}
 
@@ -155,16 +168,12 @@ do_read_syscall_test(int fd, size_t block_size) {
  *
  */
 uint64_t
-do_read_mmap_test(int fd, size_t block_size, const char *filename) {
+do_read_mmap_test(int fd, size_t block_size, size_t filesize) {
 
 	bool done = false;
 	char *mmapped_buffer = NULL;
 	int i, ret;
-	size_t filesize;
 	uint64_t begin_time, end_time, ret_token = 0;
-
-	if ((filesize = get_filesize(filename)) == -1)
-		return -1;
 
 #ifdef __MACH__
 	mmapped_buffer = (char *)mmap(NULL, filesize, PROT_READ,
@@ -174,8 +183,8 @@ do_read_mmap_test(int fd, size_t block_size, const char *filename) {
 				      MAP_PRIVATE | MAP_POPULATE, fd, 0);
 #endif
 	if (mmapped_buffer == MAP_FAILED) {
-		printf("Failed to mmap file %s of size %" PRIu64 " : %s\n",
-		       filename, (uint_least64_t)filesize, strerror(errno));
+		printf("Failed to mmap file of size %" PRIu64 " : %s\n",
+		       (uint_least64_t)filesize, strerror(errno));
 		return -1;
 	}
 
@@ -193,8 +202,7 @@ do_read_mmap_test(int fd, size_t block_size, const char *filename) {
 
 	ret = munmap(mmapped_buffer, filesize);
 	if (ret)
-		printf("Error unmapping file %s: %s\n",
-		       filename, strerror(errno));
+		printf("Error unmapping file: %s\n", strerror(errno));
 
 	return ret_token;
 }
@@ -213,4 +221,27 @@ get_filesize(const char* filename) {
 	}
 	else
 		return st.st_size;
+}
+
+void
+print_help_message(const char *progname) {
+
+	/* take only the last portion of the path */
+	const char *basename = strrchr(progname, '/');
+	basename = basename ? basename + 1 : progname;
+
+	printf("usage: %s [OPTION]\n", basename);
+	printf("  -h, --help\n"
+	       "     Print this help and exit.\n");
+	printf("  -b, --block[=BLOCKSIZE]\n"
+	       "     Block size used for read system calls.\n"
+	       "     For mmap tests, the size of the stride when iterating over the file.\n"
+	       "     Defaults to %d.\n", DEFAULT_BLOCK_SIZE);
+	printf("  -f, --file[=FILENAME]\n"
+	       "     Perform all tests on this file (defaults to %s).\n",
+	       DEFAULT_FNAME);
+	printf("  --readsyscall\n"
+	       "     Perform a read test using system calls.\n");
+	printf("  --readmmap\n"
+	       "     Perform a read test using mmap.\n");
 }
