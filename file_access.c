265a18bed49edc64e3e760083e68767f1dfd0125
Alexandra (Sasha)Fedorova <sasha@ece.ubc.ca>
2019-10-01 11:55:32 -0700

    Experiments where the file does not exist prior to being written. Random access experiment.

--- file_access.c
+++ file_access.c
@@ -21,10 +21,14 @@
 
 const char DEFAULT_FNAME[] = "/mnt/pmem/testfile";
 
-uint64_t do_read_mmap_test(int fd, size_t block_size, size_t filesize);
-uint64_t do_read_syscall_test(int fd, size_t block_size, size_t filesize);
-uint64_t do_write_mmap_test(int fd, size_t block_size, size_t filesize);
-uint64_t do_write_syscall_test(int fd, size_t block_size, size_t filesize);
+uint64_t do_read_mmap_test(int fd, size_t block_size, size_t filesize,
+			   off_t *offsets);
+uint64_t do_read_syscall_test(int fd, size_t block_size, size_t filesize,
+			      off_t *offsets);
+uint64_t do_write_mmap_test(int fd, size_t block_size, size_t filesize,
+			    int createfile, off_t *offsets);
+uint64_t do_write_syscall_test(int fd, size_t block_size, size_t filesize,
+			       off_t *offsets);
 size_t   get_filesize(const char* filename);
 void     print_help_message(const char* progname);
 
@@ -45,15 +49,21 @@ static int silent = 0;
 int main(int argc, char **argv) {
 
 	char *fname = (char*) DEFAULT_FNAME;
-	int c, fd, option_index;
-	static int read_mmap = 0, read_syscall = 0,
+	int c, fd, flags = O_RDWR, numblocks = 0, option_index;
+	static int createfile = 0, randomaccess = 0,
+		read_mmap = 0, read_syscall = 0,
 		write_mmap = 0, write_syscall = 0;
-	size_t block_size = DEFAULT_BLOCK_SIZE, filesize;
+	off_t *offsets = 0;
+	size_t block_size = DEFAULT_BLOCK_SIZE, filesize, new_file_size = -1;
 	uint64_t retval;
+
+	mode_t mode = S_IRWXU | S_IRWXG;
 
 	static struct option long_options[] =
         {
 		/* These options set a flag. */
+		{"createfile", no_argument,  &createfile, 1},
+		{"randomaccess", no_argument,  &randomaccess, 1},
 		{"readmmap", no_argument,   &read_mmap, 1},
 		{"readsyscall", no_argument,  &read_syscall, 1},
 		{"silent", no_argument,  &silent, 1},
@@ -63,12 +73,13 @@ int main(int argc, char **argv) {
 		{"block", required_argument, 0, 'b'},
 		{"file", required_argument, 0, 'f'},
 		{"help", no_argument, 0, 'h'},
+		{"size", no_argument, 0, 's'},
 		{0, 0, 0, 0}
 	};
 
 	/* Read long options */
 	while (1) {
-		c = getopt_long (argc, argv, "b:f:h",
+		c = getopt_long (argc, argv, "b:f:hs:",
 				 long_options, &option_index);
 
 		/* Detect the end of the options. */
@@ -89,22 +100,58 @@ int main(int argc, char **argv) {
 		case 'h':
 			print_help_message(argv[0]);
 			_exit(0);
+		case 's':
+			new_file_size = atoi(optarg);
+			break;
 		default:
 			break;
 		}
 	}
 
-	fd = open((const char*)fname, O_RDWR);
+	/* If the user supplied the createfile flag, they want the test
+	 * to assume that the file does not exist and create a new one.
+	 * in that case, they also must supply the target file size.
+	 * This option can only be supplied if we are doing write tests.
+	 */
+	if (createfile) {
+		if (read_mmap || read_syscall) {
+			printf("Invalid option: --createfile cannot be "
+			       "supplied with --readmmap or -readsyscall "
+			       "optons.\n");
+			print_help_message(argv[0]);
+			_exit(-1);
+		}
+		if (new_file_size < 0) {
+			printf("If --createfile option is provided, you must "
+			       "also provide the size of the new file as the "
+			       "argument to -s option.\n");
+			print_help_message(argv[0]);
+			_exit(-1);
+		}
+		flags |= O_CREAT | O_APPEND;
+	}
+
+
+	if (!silent)
+		printf("Using file %s\n", fname);
+
+	if ((filesize = get_filesize(fname)) == -1) {
+		if (read_mmap || read_syscall) {
+			printf("Cannot obtain file size for %s: %s"
+			       "File must exist prior to running read tests.\n",
+			       fname, strerror(errno));
+			_exit(-1);
+		}
+		else
+			filesize = new_file_size;
+	}
+
+	fd = open((const char*)fname, flags, mode);
 	if (fd < 0) {
-		printf("Could not open file %s: %s\n", fname, strerror(errno));
+		printf("Could not open/create file %s: %s\n",
+		       fname, strerror(errno));
 		_exit(-1);
 	}
-	else
-		if (!silent)
-			printf("Using file %s\n", fname);
-
-	if ((filesize = get_filesize(fname)) == -1)
-		_exit(-1);
 
 	if (block_size < 0 || block_size > filesize) {
 		printf("Invalid block size: %" PRIu64 " for file of size "
@@ -114,10 +161,33 @@ int main(int argc, char **argv) {
 		_exit(-1);
 	}
 
+	/*
+	 * Generate block numbers for file access. Sequential for sequential
+	 * access, random for random access.
+	 */
+	numblocks = filesize / block_size;
+	if (filesize % block_size > 0)
+		numblocks++;
+
+	offsets = (off_t *) malloc(numblocks * sizeof(off_t));
+	if (offsets == 0) {
+		printf("Failed to allocate memory: %s\n",
+		       strerror(errno));
+		_exit(-1);
+	}
+	for (int i = 0; i < numblocks; i++) {
+		int blockid;
+		if (randomaccess)
+			blockid = (int)random() % numblocks;
+		else
+			blockid = i;
+		offsets[i] = blockid * block_size;
+	}
+
 	if (read_mmap) {
 		if (!silent)
 			printf("Running readmmap test:\n");
-		retval = do_read_mmap_test(fd, block_size, filesize);
+		retval = do_read_mmap_test(fd, block_size, filesize, offsets);
 		if (!silent)
 			printf("\t Meaningless return token: %" PRIu64 "\n",
 			       retval);
@@ -125,7 +195,8 @@ int main(int argc, char **argv) {
 	if (read_syscall) {
 		if (!silent)
 			printf("Running readsyscall test:\n");
-		retval = do_read_syscall_test(fd, block_size, filesize);
+		retval = do_read_syscall_test(fd, block_size, filesize,
+					      offsets);
 		if (!silent)
 			printf("\t Meaningless return token: %" PRIu64 "\n",
 			       retval);
@@ -133,7 +204,8 @@ int main(int argc, char **argv) {
 	if (write_mmap) {
 		if (!silent)
 			printf("Running writemmap test:\n");
-		retval = do_write_mmap_test(fd, block_size, filesize);
+		retval = do_write_mmap_test(fd, block_size, filesize,
+					    createfile, offsets);
 		if (!silent)
 			printf("\t Meaningless return token: %" PRIu64 "\n",
 			       retval);
@@ -141,7 +213,9 @@ int main(int argc, char **argv) {
 	if (write_syscall) {
 		if (!silent)
 			printf("Running writesyscall test:\n");
-		retval = do_write_syscall_test(fd, block_size, filesize);
+		retval = do_write_syscall_test(fd, block_size, filesize,
+					       offsets);
+
 		if (!silent)
 			printf("\t Meaningless return token: %" PRIu64 "\n",
 			       retval);
@@ -160,32 +234,37 @@ int main(int argc, char **argv) {
  *
  */
 uint64_t
-do_syscall_test(int fd, size_t block_size, size_t filesize, char optype);
+do_syscall_test(int fd, size_t block_size, size_t filesize, char optype,
+		off_t *offsets);
 
 uint64_t
-do_read_syscall_test(int fd, size_t block_size, size_t filesize) {
+do_read_syscall_test(int fd, size_t block_size, size_t filesize,
+		     off_t *offsets) {
 
-	return do_syscall_test(fd, block_size, filesize, READ);
+	return do_syscall_test(fd, block_size, filesize, READ, offsets);
 }
 
 uint64_t
-do_write_syscall_test(int fd, size_t block_size, size_t filesize) {
+do_write_syscall_test(int fd, size_t block_size, size_t filesize,
+		      off_t *offsets) {
 
-	return do_syscall_test(fd, block_size, filesize, WRITE);
+	return do_syscall_test(fd, block_size, filesize, WRITE, offsets);
 }
 
 
 uint64_t
-do_syscall_test(int fd, size_t block_size, size_t filesize, char optype) {
+do_syscall_test(int fd, size_t block_size, size_t filesize, char optype,
+		 off_t *offsets) {
 
 	bool done = false;
 	char *buffer = NULL;
+	int i = 0;
 	size_t total_bytes_transferred = 0;
 	uint64_t begin_time, end_time, ret_token = 0;
 
 	buffer = (char*)malloc(block_size);
 	if (buffer == NULL) {
-		printf("Failed to allocate a buffer: %s\n", strerror(errno));
+		printf("Failed to allocate memory: %s\n", strerror(errno));
 		return -1;
 	}
 
@@ -195,10 +274,14 @@ do_syscall_test(int fd, size_t block_size, size_t filesize, char optype) {
 
 		size_t bytes_transferred = 0;
 
+		off_t offset = offsets[i++];
+
 		if (optype == READ)
-			bytes_transferred = read(fd, buffer, block_size);
+			bytes_transferred = pread(fd, buffer, block_size,
+						  offset);
 		else if (optype == WRITE)
-			bytes_transferred = write(fd, buffer, block_size);
+			bytes_transferred = pwrite(fd, buffer, block_size,
+						  offset);
 
 		if (bytes_transferred == 0)
 			done = true;
@@ -216,13 +299,15 @@ do_syscall_test(int fd, size_t block_size, size_t filesize, char optype) {
 			/* Pretend that we actually use the data */
 			ret_token += buffer[0];
 		}
+		if (i*block_size >= filesize)
+			done = true;
 	}
 
 	end_time = nano_time();
 
 	if (!silent)
 		printf("%s: %" PRIu64 " bytes transferred in %" PRIu64 ""
-		       "ns.\n", (optype == READ)?"readsyscall":"writesyscall",
+		       " ns.\n", (optype == READ)?"readsyscall":"writesyscall",
 		       (uint_least64_t)total_bytes_transferred,
 		       (end_time-begin_time));
 	printf("\t %.2f GB/second\n",
@@ -237,28 +322,43 @@ do_syscall_test(int fd, size_t block_size, size_t filesize, char optype) {
  *
  */
 
-uint64_t do_mmap_test(int fd, size_t block_size, size_t filesize, char optype);
+uint64_t do_mmap_test(int fd, size_t block_size, size_t filesize, char optype,
+		      int createfile, off_t *offsets);
 
 uint64_t
-do_read_mmap_test(int fd, size_t block_size, size_t filesize) {
+do_read_mmap_test(int fd, size_t block_size, size_t filesize, off_t *offsets) {
 
-	return do_mmap_test(fd, block_size, filesize, READ);
+	return do_mmap_test(fd, block_size, filesize, READ, 0, offsets);
 }
 
 uint64_t
-do_write_mmap_test(int fd, size_t block_size, size_t filesize) {
+do_write_mmap_test(int fd, size_t block_size, size_t filesize,
+		   int createfile, off_t *offsets) {
 
-	return do_mmap_test(fd, block_size, filesize, WRITE);
+	return do_mmap_test(fd, block_size, filesize, WRITE, createfile,
+			    offsets);
 }
 
 uint64_t
-do_mmap_test(int fd, size_t block_size, size_t filesize, char optype) {
+do_mmap_test(int fd, size_t block_size, size_t filesize, char optype,
+	     int createfile, off_t *offsets) {
 
 
 	bool done = false;
 	char *mmapped_buffer = NULL, *buffer = NULL;
-	int i, j, ret;
+	int i, j, numblocks, ret;
 	uint64_t begin_time, end_time, ret_token = 0;
+
+	if (createfile) {
+
+		ret = ftruncate(fd, filesize);
+		if (ret) {
+			printf("Failed to truncate the file to size "
+			       "%" PRIu64 " : %s\n",
+			       (uint_least64_t)filesize, strerror(errno));
+			return -1;
+		}
+	}
 
 #ifdef __MACH__
 	mmapped_buffer = (char *)mmap(NULL, filesize,
@@ -277,20 +377,24 @@ do_mmap_test(int fd, size_t block_size, size_t filesize, char optype) {
 
 	buffer = (char*)malloc(block_size);
 	if (buffer == NULL) {
-		printf("Failed to allocate a buffer: %s\n", strerror(errno));
+		printf("Failed to allocate memory: %s\n", strerror(errno));
 		return -1;
 	}
 	memset((void*)buffer, 0, block_size);
 
 	begin_time = nano_time();
 
-	for (i = 0; i < filesize; i += block_size) {
+	for (i = 0; i < filesize/block_size; i++) {
+		off_t offset = offsets[i];
+
 		if (optype == READ) {
-			memcpy(buffer, &mmapped_buffer[i], block_size);
+			memcpy(buffer, &mmapped_buffer[offset],
+			       block_size);
 			ret_token += buffer[0];
 		}
 		else if (optype == WRITE) {
-			memcpy(&mmapped_buffer[i], buffer, block_size);
+			memcpy(&mmapped_buffer[offset], buffer,
+			       block_size);
 			ret_token += mmapped_buffer[i];
 		}
 	}
@@ -319,11 +423,8 @@ get_filesize(const char* filename) {
 
 	struct stat st;
 	retval = stat(filename, &st);
-	if (retval) {
-		printf("Failed to find the size of file %s:"
-		       "%s\n", filename, strerror(errno));
+	if (retval)
 		return -1;
-	}
 	else
 		return st.st_size;
 }
@@ -340,13 +441,23 @@ print_help_message(const char *progname) {
 	       "     Print this help and exit.\n");
 	printf("  -b, --block[=BLOCKSIZE]\n"
 	       "     Block size used for read system calls.\n"
-	       "     For mmap tests, the size of the stride when iterating over the file.\n"
+	       "     For mmap tests, the size of the stride when iterating "
+	       "     over the file.\n"
 	       "     Defaults to %d.\n", DEFAULT_BLOCK_SIZE);
 	printf("  -f, --file[=FILENAME]\n"
 	       "     Perform all tests on this file (defaults to %s).\n",
 	       DEFAULT_FNAME);
+	printf("  --createfilel\n"
+	       "     The test assumes that the file does not exist and will "
+	       "     create it. This option is valid only with read tests. "
+	       "     If using this option you must also supply the file size "
+	       "     as the argument to -s option.\n");
 	printf("  --readsyscall\n"
 	       "     Perform a read test using system calls.\n");
 	printf("  --readmmap\n"
 	       "     Perform a read test using mmap.\n");
+	printf("  --writesyscall\n"
+	       "     Perform a write test using system calls.\n");
+	printf("  --writemmap\n"
+	       "     Perform a write test using mmap.\n");
 }
